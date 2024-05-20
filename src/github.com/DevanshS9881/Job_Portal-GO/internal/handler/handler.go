529c286fa94cac85cba00b5dc088b957e6d60da4
Devansh Srivastava <devansh@dev.glug.com>
2024-05-21 00:52:45 +0530

    comitted

--- src/github.com/DevanshS9881/Job_Portal-GO/internal/handler/handler.go
+++ src/github.com/DevanshS9881/Job_Portal-GO/internal/handler/handler.go
@@ -3,13 +3,13 @@ import(
 	"time"
     "github.com/gofiber/fiber/v2"
 	jtoken"github.com/golang-jwt/jwt/v4"
-	"github.com/DevanshS9881/Job_Portal-G0/config"
-	"github.com/DevanshS9881/Job_Portal-G0/models"
-	"github.com/DevanshS9881/Job_Portal-G0/repository"
+	"github.com/DevanshS9881/Job_Portal-G0/internal/config"
+	"github.com/DevanshS9881/Job_Portal-G0/internal/models"
+	"github.com/DevanshS9881/Job_Portal-G0/internal/repository"
 )
 func Login(c *fiber.Ctx) error{
 	loginRequest:=new(models.LoginRequest)
 	if err:=c.BodyParser(loginRequest);err!=nil{
-		return c.Status(fiber.StatusBadRequest).JSON
+		return c.Status(fiber.StatusBadRequest).JS
 	}
 }
